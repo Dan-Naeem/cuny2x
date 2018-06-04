8e63c1a29f0a657d3288df39ce3a2bd0ce703911
Dan Naeem <dan.naeem95@gmail.com>
2018-06-03 23:39:26 -0400

    added day3 assignment

--- assignments/day3/photoAlbum.js
+++ assignments/day3/photoAlbum.js
@@ -1,10 +1,43 @@
+/*
+
+Implement an object model that allows you to store strings that represent a Photo.
+
+Your model should include an Album object that can contain many Photo objects in its photos attribute.
+
+Each Album should allow you to add a new photo, list all photos, and access a specific photo by the order it was added.
+
+Each Photo should tell you the photo's file name and the location the photo was taken in.
+
+Create instances of each object defined to prove that your object model works.
+
+*/
+
 //create a constructor for the photo object
 function Photo(name, location){
   this.name = name;
-  this.name = location;
+  this.location = location;
 }
 
-Photo.prototype.details = function() {
-  return this.name + " " + this.location;
-  //
+function Album() {
+  var allPhotos = [];
 }
+
+Album.prototype.add = function(Photo) {
+  this.allPhotos.push(aPic);
+}
+
+Album.prototype.display = function() {
+  for (i = 0; i < this.list.length; i++) {
+    console.log(allPhotos[i]);
+  }
+}
+
+let pic1 = new Photo("thur", "park");
+let pic2 = new Photo("fri", "school");
+console.log(pic1.name + " : " + pic1.location);
+console.log(pic2.name + " : " + pic2.location);
+
+let album1 = new Album();
+album1.add(pic1);
+album1.add(pic2);
+album1.display;
